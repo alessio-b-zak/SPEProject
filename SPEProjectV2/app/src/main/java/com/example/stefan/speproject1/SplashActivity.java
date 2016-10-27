9ddc733cacd49208ace7d3f2efa40d04af8305d9
Stefan <sk15962@bristol.ac.uk>
2016-10-27 14:22:38 +0100

    New app verision added called SPEProjectV2

---
+++ SPEProjectV2/app/src/main/java/com/example/stefan/speproject1/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.stefan.speproject1;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DataPullTask task = new DataPullTask();
+
+        task.execute();
+    }
+
+    private class DataPullTask extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
+            TODO: // BACK END COULD PULL DATA HERE
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Intent intent = new Intent(SplashActivity.this, MapsActivity.class);
+            SplashActivity.this.startActivity(intent);
+            SplashActivity.this.finish();
+        }
+
+    }
+
+
+}
+
+
