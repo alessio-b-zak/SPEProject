93ce76d2a0cdcc6e668dd6659244b8ecb9879513
Stefan <sk15962@bristol.ac.uk>
2016-11-02 19:11:56 +0000

    Basic GPS button working. Fails if user is not connected to internet or issue with multiple icons appearing on map

--- android/app/src/main/java/com/bitbusters/android/speproject/MapsActivity.java
+++ android/app/src/main/java/com/bitbusters/android/speproject/MapsActivity.java
@@ -1,9 +1,14 @@
 package com.bitbusters.android.speproject;
 
 import android.*;
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -12,18 +17,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     private FloatingActionButton mCamButton;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLocation;
+    private boolean connected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +56,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        // Create an instance of GoogleAPIClient -> Required for the GPS Location
+         if(mGoogleApiClient == null) {
+             mGoogleApiClient = new GoogleApiClient.Builder(this)
+                     .addConnectionCallbacks(this)
+                     .addOnConnectionFailedListener(this)
+                     .addApi(LocationServices.API)
+                     .build();
+         }
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
 
@@ -74,7 +107,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+
+
+    }
+
+    // GPS Location Code
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            connected = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+    }
+
+    public void currentLocation(View v){
+        if(connected) {
+            mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            double longitude = mLocation.getLongitude();
+            double latitude = mLocation.getLatitude();
+            mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).icon(BitmapDescriptorFactory.fromResource(R.drawable.target_icon)));
+            CameraPosition newcameraPosition = new CameraPosition.Builder().zoom(10).target(new LatLng(latitude,longitude)).build();
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(newcameraPosition));
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            if(grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // We can now safely use the API we requested access to
+                    connected = true;
+            } else {
+                // Permission was denied or request was cancelled
+            }
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("LocationAPI", "Connection Suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i("LocationAPI", "Connection failed. Error: " + connectionResult.getErrorCode());
     }
 }
