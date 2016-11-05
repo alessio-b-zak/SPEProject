ef2c093461f37e27c44a360e0f53e5cbbb564f97
Mihajlo Milosavljevic <miksa108@gmail.com>
2016-11-05 13:25:44 +0000

    Add getSamplingPoints

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,20 +8,38 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var wims = new XMLHttpRequest();
 
-wims.open("GET", "http://environment.data.gov.uk/water-quality/id/sampling-point?_limit=60000", false);
-wims.send();
+router.get('/getSamplingPoints/:lat/:lon', function(req, res) {
+	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	var wims = new XMLHttpRequest();
 
-// status 200 = OK
-console.log(wims.status);
-console.log(wims.statusText);
-//console.log(wims.responseText);
+	wims.open("GET", "http://environment.data.gov.uk/water-quality/id/sampling-point?lat=" \
+			  + req.params.lat + "&long=" + req.params.lon + "&dist=10&samplingPointStatus=open", false);
+	wims.send();
 
-var json = JSON.parse(wims.responseText);
-var count = Object.keys(json.items).length;
-console.log(count);
+	// status 200 = OK
+	console.log(wims.status);
+	console.log(wims.statusText);
+
+	var json = JSON.parse(wims.responseText);
+	delete json["@context"];
+	delete json["meta"];
+	for (var i = 0; i < json.items.length; i++){
+		delete json.items[i].area;
+		delete json.items[i].comment;
+		delete json.items[i].easting;
+		delete json.items[i].northing;
+		delete json.items[i].notation;
+		delete json.items[i].label;
+		delete json.items[i].samplingPointStatus;
+		delete json.items[i].subArea;
+		json.items[i].samplingPointType = json.items[i].samplingPointType.label;
+	}
+	//console.log(json);
+	//var count = Object.keys(json.items).length;
+	//console.log(count);
+	res.send(json);
+});
 
 //Get the file contents
 // TODO: fix ReferenceError: File is not defined
