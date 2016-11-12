09c2d8834f4533c1a999362058d2392a1bc05270
Mihajlo Milosavljevic <miksa108@gmail.com>
2016-11-12 15:30:03 +0000

    Add image collection

--- server/routes/db.js
+++ server/routes/db.js
@@ -6,7 +6,33 @@ var server = new Server('localhost', 27017, {auto_reconnect: true});
 var db = new Db('exampleDb', server);
 
 db.open(function(err, db) {
-  if(!err) {
+  if(err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
     console.log("We are connected");
+	//Create a collection if it does not exist.
+	var images = db.collection("images");	  
+	var image_1 = {comment: 'codrin is gay', loc: {long : '-2.611741', lat : '51.448787'}}
+	var image_2 = {comment: 'codrin is really gay', loc: {long : '-3.611741', lat : '52.448787'}}
+	images.insert([image_1, image_2]);
+	
+	images.find(
+	   {
+		 loc: {
+		   $geoWithin: {
+			  $polygon: [ [ -2.634702, 51.469556 ], 
+						  [ -2.609305, 51.456596 ],
+						  [ -2.598833, 51.442560 ],
+						  [ -2.623243, 51.443339 ] ]
+			}
+		 }
+	   }
+	).toArray(function (err, result) {
+		if (err) {
+        console.log(err);
+      } else {
+        console.log('Found:', result);
+      }
+	});
   }
 });
